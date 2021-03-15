8653fb34bffb8b8e2247c67873b94195e10a8c36
Millad Ayoubi <Milladayoubi@gmail.com>
2021-03-15 00:19:38 -0400

    Spinning Link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello From React App</h1>
+      <div className="linkApp"> 
+      <a className="link" href="https://portfolio-ayoubi.netlify.app/">Millad's Portfolio</a>
+      </div>
     </div>
   );
 }
