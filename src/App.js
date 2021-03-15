2a1b0e3ce24cef9f9e063a9aa359aec83d88cd3e
Millad Ayoubi <Milladayoubi@gmail.com>
2021-03-15 00:42:11 -0400

    Exporting Components To Othe Files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 
 import './App.css';
 
+import Header from './header';
+
 function App() {
+  const greeting = 'Hello'
   return (
     <div className="App">
+      <div className="container">
+   
       <h1>Hello From React App</h1>
+      <h2>{greeting} There!</h2>
       <div className="linkApp"> 
       <a className="link" href="https://portfolio-ayoubi.netlify.app/">Millad's Portfolio</a>
       </div>
+      </div>
+
+      
+      <Header/>
+      
     </div>
   );
 }
 
 export default App;
+
+
+
